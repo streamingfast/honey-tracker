643ecdd967e96cf2b257ec630d106b72cce8b47c
billettc <charles@cbillette.com>
2023-07-11 08:19:02 -0400

    added some todos

--- data/db.go
+++ data/db.go
@@ -9,7 +9,12 @@ type DB interface {
 	HandlePayment(payment *proto.Payment) error
 	HandleSplitPayment(splitPayment *proto.SplitPayment) error
 	HandleTransfer(transfer *proto.Transfer) error
-	HandleMint(transfer *proto.Mint) error
-	HandleBurn(transfer *proto.Mint) error
+	HandleMint(mint *proto.Mint) error
+	HandleBurn(burn *proto.Mint) error
+	TransferChecked(transferCheck *proto.TransferCheck) error
 	//todo: what else?
+	//todo: MintToChecked
+	//todo: BurnChecked
+
+	//todo: double check https://github.com/solana-labs/solana-program-library/blob/master/token/program/src/instruction.rs
 }
