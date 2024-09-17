b9fc999a5cf12a195692e439770e442ccb7f3a73
Charles Billette <charles@cbillette.com>
2024-09-17 11:08:31 -0400

    Add left join to exclude fleet addresses from results
    Previously, the query included payments to all addresses, potentially cluttering results with fleet addresses. This change introduces a left join on the 'fleets' table and filters out non-null fleet addresses, thus refining the selection to non-fleet payees only.

--- dbt/hivemapper/models/example/dbt_driver_and_revenu.sql
+++ dbt/hivemapper/models/example/dbt_driver_and_revenu.sql
@@ -2,9 +2,11 @@
 
 select
     RANK() OVER(ORDER BY SUM(ap.fleet_payment) DESC) rank,
-     da.address,
+    da.address,
     sum(ap.fleet_payment) as honey,
     sum(ap.fleet_usd_payment) as USD
 from hivemapper.dbt_all_payments ap
-     inner join hivemapper.derived_addresses da on da.derivedaddress = ap.payee_address
+         inner join hivemapper.derived_addresses da on da.derivedaddress = ap.payee_address
+         LEFT JOIN hivemapper.fleets f ON da.address = f.address
+where f.address is null
 group by da.address
