704732912a79d248e4146be679011a8dabbd872b
Charles Billette <charles@cbillette.com>
2024-09-17 10:13:17 -0400

    Refactor SQL query for revenue ranking
    This change updates the query to use `derived_addresses.address` instead of `fleets.address` and simplifies join conditions. The grouped column and join statements have been adjusted to improve query clarity and readability.

--- dbt/hivemapper/models/example/dbt_driver_and_revenu.sql
+++ dbt/hivemapper/models/example/dbt_driver_and_revenu.sql
@@ -2,10 +2,9 @@
 
 select
     RANK() OVER(ORDER BY SUM(ap.fleet_payment) DESC) rank,
-    f.address,
+     da.address,
     sum(ap.fleet_payment) as honey,
     sum(ap.fleet_usd_payment) as USD
-from hivemapper.fleets f
-     inner join hivemapper.derived_addresses da on da.address = f.address
-     inner join hivemapper.dbt_all_payments ap on ap.payee_address = da.derivedaddress
-group by f.address
+from hivemapper.dbt_all_payments ap
+     inner join hivemapper.derived_addresses da on da.derivedaddress = ap.payee_address
+group by da.address
